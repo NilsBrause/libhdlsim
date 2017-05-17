349b27bd62560887395e6758dd50f2965a816960
Nils Christopher Brause <nilschrbrause@googlemail.com>
2017-05-17 16:16:44 +0200

    Improved example.

--- example.cpp
+++ example.cpp
@@ -33,54 +33,76 @@
 
 using namespace hdl;
 
-int main()
+template<unsigned int bits = 9>
+class example_t
 {
-  const unsigned int bits = 9;
-  
+private:
+  // declare signals
   wire<std_logic> clk;
   wire<std_logic> reset;
-
-  wire<fixed_t<true, bits, 0>> one(1), tmp, count;
+  wire<fixed_t<true, bits, 0>> one, tmp, count;
   wire<fixed_t<true, 2*bits, 0>> square;
-  
-  add(count, one, tmp);
-  reg(clk, reset, wire<std_logic>(1), tmp, count);
-  mul(count, count, square);
 
-  print(clk);
-  print(reset);
-  print(count);
-  print(square);
+  // implement testbench
+  part testbench;
+  void tb_func(uint64_t time)
+  {
+    // create clock 
+    switch(time % 2)
+      {
+      case 0:
+        clk = 0;
+        break;
+      case 1:
+        clk = 1;
+        break;
+      }
 
-  clk.setname("clk");
-  reset.setname("reset");
-  one.setname("one");
-  tmp.setname("tmp");
-  count.setname("count");
-  square.setname("square");
+    // create active low reset
+    if(time < 10)
+      reset = 0;
+    else
+      reset = 1;
+  }
 
-  part testbench = part({ },
-                        { clk, reset },
-                        [=] (uint64_t time)
-                        {
-                          switch(time % 2)
-                            {
-                            case 0:
-                              clk = 0;
-                              break;
-                            case 1:
-                              clk = 1;
-                              break;
-                            }
+public:
+  example_t()
+  {
+    // set upinitial values
+    one = 1;
 
-                          if(time < 10)
-                            reset = 0;
-                          else
-                            reset = 1;
-                        });
+    // connect components
+    add(count, one, tmp);
+    reg(clk, reset, wire<std_logic>(1), tmp, count);
+    mul(count, count, square);
 
-  simulator sim(testbench);
-  sim.run(100);
-  
+    // give names to interesting signals
+    clk.setname("clk");
+    reset.setname("reset");
+    count.setname("count");
+    square.setname("square");
+
+    // print interesting signals
+    print(clk);
+    print(reset);
+    print(count);
+    print(square);
+
+    // create testbench part
+    testbench = part({ }, { clk, reset }, [this] (uint64_t time) { this->tb_func(time); });
+  }
+
+  void run(unsigned int duration)
+  {
+    // create and run simulation
+    simulator sim(testbench);
+    sim.run(duration);
+  }
+};
+
+int main()
+{
+  example_t<> example;
+  example.run(100);
   return 0;
 }
